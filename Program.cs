717004857707c489603646c3a68c7a77f27482fd
Yazan Alsolyman <132354795+4yznx@users.noreply.github.com>
2024-12-07 15:18:12 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,41 @@
-using BarberShop.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using BarberShop.Data;
+using BarberShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Database
+// Configure Database (SQL Server)
 builder.Services.AddDbContext<BarberDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
 
+// Configure Identity
+builder.Services.AddIdentity<Kullanici, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+})
+  .AddEntityFrameworkStores<BarberDbContext>()
+  .AddDefaultTokenProviders();
+
+
+
+// Configure Application Cookie
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+    options.ExpireTimeSpan = TimeSpan.FromDays(14);
+    options.SlidingExpiration = true;
+});
 
 var app = builder.Build();
 
@@ -17,7 +43,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -26,6 +51,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
